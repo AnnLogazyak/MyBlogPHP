0405f95ec2da20c02c2ddfa9ece05481304085f2
AnnLogazyak <165693225+AnnLogazyak@users.noreply.github.com>
2025-05-18 10:47:49 +0500

    Добавлена заготовка для работы с категориями

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(["namespace"=> 'App\Http\Controllers\Main'], function () {
-    Route::get("/","IndexController")->name("index");
+    Route::get("/","IndexController");
 });
 
 Route::group(["namespace" => 'App\Http\Controllers\Admin', 'prefix'=> 'admin'], function () {
     Route::group(["namespace" => 'Main'], function () {
-        Route::get("/", "IndexController")->name("index");
+        Route::get("/", "IndexController");
+    });
+
+    Route::group(["namespace" => 'Category', 'prefix' => 'categories'], function () {
+        Route::get("/", "IndexController");
     });
 });
 
